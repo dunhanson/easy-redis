9321594164ee1f6f256df63c0385e8a6c612e271
dunhanson <dunhanson@gmail.com>
2020-06-17 14:59:30 +0800

    gson工具类

---
+++ src/main/java/site/dunhanson/redis/utils/GsonUtils.java
@@ -0,0 +1,12 @@
+package site.dunhanson.redis.utils;
+
+import com.google.gson.Gson;
+
+/**
+ * @author dunhanson
+ * @date 2020-06-17
+ * @description gson工具类
+ */
+public class GsonUtils {
+    public static Gson gson = new Gson();
+}
