967a3007b42916c604da04936a161c1fa0b05b51
dunhanson <dunhanson@gmail.com>
2020-06-17 14:59:50 +0800

    object测试方法

---
+++ src/test/java/ObjectUtilsTest.java
@@ -0,0 +1,37 @@
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.junit.Test;
+import site.dunhanson.redis.utils.ObjectUtils;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ObjectUtilsTest {
+
+    @Test
+    public void stringTest() {
+        // map对象
+        String str = "hello,world";
+        // 对象转二进制
+        byte[] bytes = ObjectUtils.toByteArray(str);
+        // 二进制转对象
+        str = ObjectUtils.toEntity(bytes, String.class);
+        System.out.println(str);
+    }
+
+    @Test
+    public void listTest() {
+        // map对象
+        List<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        // 对象转二进制
+        byte[] bytes = ObjectUtils.toByteArray(list);
+        // 二进制转对象
+        Type type = new TypeToken<List<String>>(){}.getType();
+        list = ObjectUtils.toEntity(bytes, type);
+        System.out.println(list);
+    }
+
+}
